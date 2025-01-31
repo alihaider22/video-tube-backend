400f31e09d238a149e60c387d64828107f3c0156
Ali Haider <alihaiderawan528@gmail.com>
2025-01-31 09:34:03 +0500

    added bcrypt for securing passowrd

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { model } from "mongoose";
+import bcrypt from "bcrypt";
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
@@ -48,6 +49,23 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = model("User", userSchema);
 
